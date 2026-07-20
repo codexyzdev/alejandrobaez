f83ffc35d10b282e4cef460a9d6f1029ab948177
codexyzdev <alejandrobaez938@gmail.com>
2026-07-20 09:11:50 -0400

    Añadir sección de portafolio con diseño responsivo y funcionalidad de filtrado

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,23 +15,42 @@
 /*==================== SCROLL ABOUT ANIMATION ====================*/
 gsap.registerPlugin(ScrollTrigger);
 gsap.utils.toArray(".text-gradient").forEach((span) => {
-  gsap.to(span,{
-    backgroundSize:'100% 100%',
-    ease:'none',
-    scrollTrigger:{
+  gsap.to(span, {
+    backgroundSize: "100% 100%",
+    ease: "none",
+    scrollTrigger: {
       trigger: span,
-      start:'top bottom',
-      end:'top center',
-      scrub:true
-    }
-  })
-});;
+      start: "top bottom",
+      end: "top center",
+      scrub: true,
+    },
+  });
+});
 
 /*==================== DARK LIGHT THEME ====================*/
 
 /*==================== MIXITUP FILTER PORTFOLIO ====================*/
 
+var mixer = mixitup(".work-container", {
+  selectors: {
+    target: ".mix",
+  },
+  animation: {
+    duration: 300,
+  },
+});
+
 /* Active work */
+const linkWork = document.querySelectorAll(".work-item");
+
+function activeWork() {
+  linkWork.forEach((a) => {
+    a.classList.remove("active-work");
+  });
+  this.classList.add("active-work");
+}
+
+linkWork.forEach((a) => a.addEventListener("click", activeWork));
 
 /*==================== EMAIL JS ====================*/
 
