11a4016af70a3756632e653396d76e4bcfad00d0
codexyzdev <alejandrobaez938@gmail.com>
2026-07-20 20:56:38 -0400

    Añadir footer con enlaces a redes sociales y copyright

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,14 @@
 /*==================== REMOVE MENU MOBILE ====================*/
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 20
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+
+window.addEventListener("scroll", scrollHeader);
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
